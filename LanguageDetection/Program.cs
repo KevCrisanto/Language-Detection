2f7762dda48516bc5361bf2c901bf26bf6260f87
Kevin <kevincrisanto@gmail.com>
2018-03-16 13:16:39 -0700

    Removed "test" option

--- LanguageDetection/Program.cs
+++ LanguageDetection/Program.cs
@@ -16,10 +16,10 @@ namespace LanguageDetection
                 var decision = string.Empty;
                 try
                 {
-                    Console.WriteLine(@"The program can detect a language, learn a new language, or simply test the current supported languages.\n ");
+                    Console.WriteLine("The program can detect a language, learn a new language, or simply test the current supported languages.");
                     Console.WriteLine(@"- To detect a language just type the path to the file with the text. Ex: C:\text.txt");
                     Console.WriteLine(@"- To learn a language type the language code and the path to the file with the sample text. Ex: pl C:\polish.txt");
-                    Console.WriteLine("- Press Enter if you just want to test the program (it may take several seconds)");
+                    Console.Write("Enter your command: ");
                     string choice = (Console.ReadLine() ?? string.Empty);
 
                     if (choice != string.Empty)
@@ -37,7 +37,7 @@ namespace LanguageDetection
                     }
                     else
                     {
-                        Test();
+                        Console.WriteLine("Please enter a valid action.");
                     }
                 }
                 catch (Exception ex)
@@ -50,10 +50,10 @@ namespace LanguageDetection
                     decision = (Console.ReadLine() ?? string.Empty);
                 } while (!decision.Equals("Y", StringComparison.OrdinalIgnoreCase) &&
                     !decision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
                 continueDecision = decision.Equals("Y", StringComparison.OrdinalIgnoreCase);
 
             } while (continueDecision);
-            Console.ReadKey();
         }
 
         static void Learn(string languageCode, string newLanguageFile, string knownLanguagesFile)
@@ -78,67 +78,6 @@ namespace LanguageDetection
             var languageCode = detector.Detect(file, out score);
 
             Console.WriteLine("The language code of the detected language is: {0} ({1})", languageCode, score);
-        }
-
-        static void Test()
-        {
-            const string testFilesPath = @"lang\";
-
-            var learner = new LanguageLearner();
-
-            var english = learner.Learn("en", Path.Combine(testFilesPath, "en-source.txt"));
-
-            var dutch = learner.Learn("nl", Path.Combine(testFilesPath, "nl-source.txt"));
-
-            var spanish = learner.Learn("es", Path.Combine(testFilesPath, "es-source.txt"));
-
-            var bulgarian = learner.Learn("bg", Path.Combine(testFilesPath, "bg-source.txt"));
-
-            var russian = learner.Learn("ru", Path.Combine(testFilesPath, "ru-source.txt"));
-
-            var german = learner.Learn("de", Path.Combine(testFilesPath, "de-source.txt"));
-
-            var languages = new Dictionary<string, Dictionary<string, int>>
-                {
-                    { "en", english },
-                    { "nl", dutch },
-                    { "es", spanish },
-                    { "bg", bulgarian },
-                    { "ru", russian },
-                    { "de", german },
-                };
-
-            var detector = new LanguageDetector(languages);
-
-            int scoreEnglish;
-            var testEnglish = detector.Detect(Path.Combine(testFilesPath, "en-sample.txt"), out scoreEnglish);
-
-            int scoreDutch;
-            var testDutch = detector.Detect(Path.Combine(testFilesPath, "nl-sample.txt"), out scoreDutch);
-
-            int scoreSpanish;
-            var testSpanish = detector.Detect(Path.Combine(testFilesPath, "es-sample.txt"), out scoreSpanish);
-
-            int scoreBulgarian;
-            var testBulgarian = detector.Detect(Path.Combine(testFilesPath, "bg-sample.txt"), out scoreBulgarian);
-
-            int scoreRussian;
-            var testRussian = detector.Detect(Path.Combine(testFilesPath, "ru-sample.txt"), out scoreRussian);
-
-            int scoreGerman;
-            var testGerman = detector.Detect(Path.Combine(testFilesPath, "de-sample.txt"), out scoreGerman);
-
-            Console.WriteLine("Test 1: {0} ({1})", testEnglish, scoreEnglish);
-
-            Console.WriteLine("Test 2: {0} ({1})", testDutch, scoreDutch);
-
-            Console.WriteLine("Test 3: {0} ({1})", testSpanish, scoreSpanish);
-
-            Console.WriteLine("Test 4: {0} ({1})", testBulgarian, scoreBulgarian);
-
-            Console.WriteLine("Test 5: {0} ({1})", testRussian, scoreRussian);
-
-            Console.WriteLine("Test 6: {0} ({1})", testGerman, scoreGerman);
         }
     }
 }
